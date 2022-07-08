eecaed43e1589e7ee33eea2688a6bc4f0e87a9f6
Valerio <canivalerio93@gmail.com>
2022-07-08 19:28:59 +0200

    le freccette funzionanoooo!!!

--- js/main.js
+++ js/main.js
@@ -11,16 +11,36 @@ let carousel = document.getElementById('carousel');
 for( let i=0; i<carouselImages.length; i++){
     
     carousel.innerHTML +=`<div class="image">
-    <img src="${carouselImages[i]}">
-    </div>`;
-    console.log(i);
-    
-}
+                            <img src="${carouselImages[i]}">
+                        </div>`;
+};
 
-const imageContainer = document.querySelectorAll('.image')
+const imageContainer = document.querySelectorAll('.image');
+
+let prev = document.getElementById('prev').addEventListener('click', goUp);
+let next = document.getElementById('next').addEventListener('click', goDown);
 let imageSelector = 0;
+
 imageContainer[imageSelector].classList.add('show');
 
-let prev = getElementById('prev');
-let next = getElementById('next');
+function goUp(event){
+    for(let x=0; x<1; x++ ){
+        imageContainer[imageSelector].classList.remove('show');
+        imageSelector--;
+        
+        imageContainer[imageSelector].classList.add('show');
+    }
+};
+
+function goDown(event){
+    for(let x=0; x<1; x++ ){
+        imageContainer[imageSelector].classList.remove('show');
+        imageSelector++;
+        imageContainer[imageSelector].classList.add('show');
+    }
+    
+};
+
+
+
 
